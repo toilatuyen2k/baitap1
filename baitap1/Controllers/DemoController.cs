c4e24e3a74ad3a4ccc831115536d6c85e6f14197
ACER <toilatuyen2k@gmail.com>
2021-08-31 13:11:40 +0700

    Add project files.

---
+++ baitap1/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using baitap1.Models;
+namespace baitap1.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        Class1 GPT = new Class1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
+            double kq = GPT.GiaiPT(so1, so2);
+            ViewBag.KetQua = kq;
+            return View();
+        }
+    }
+}
