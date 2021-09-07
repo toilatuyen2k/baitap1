b2bad756c79f1b1f9bd8bcd5b56b86f4cda9d346
ACER <toilatuyen2k@gmail.com>
2021-09-07 09:24:44 +0700

    avc

--- baitap1/Controllers/DemoController.cs
+++ baitap1/Controllers/DemoController.cs
@@ -4,23 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using baitap1.Models;
+
 namespace baitap1.Controllers
 {
     public class DemoController : Controller
     {
+        //khai bao doi tuong can ke thua
+        GiaiPhuongTrinh Gpt = new GiaiPhuongTrinh();
+
         // GET: Demo
-        Class1 GPT = new Class1();
-        public ActionResult Index()
+        public ActionResult Timx()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Index(string soA, string soB)
+        public ActionResult Timx(string soA, string soB)
         {
             double so1 = Convert.ToDouble(soA);
             double so2 = Convert.ToDouble(soB);
-            double kq = GPT.GiaiPT(so1, so2);
-            ViewBag.KetQua = kq;
+            //ke thua phong thuc TimX
+            double gt_x = Gpt.Timx(so1, so2);
+            ViewBag.timgtx = gt_x;
             return View();
         }
     }
---
+++ baitap1/Models/Classdungdekethua.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap1.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double Timx(double a, double b)
+        {
+            double gtx = 0;
+            gtx = -b / a;
+            return gtx;
+        }
+    }
+}
