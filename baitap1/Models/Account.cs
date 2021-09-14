737a9e87ca8837cdd510aa6778a67b0ddd8a2070
ACER <toilatuyen2k@gmail.com>
2021-09-14 09:21:29 +0700

    a

---
+++ baitap1/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace baitap1.Models
+{
+    [Table("Accounts")]
+    public class Account
+    {
+        [Key]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ baitap1/Models/LTQLDbcontext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace baitap1.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext() : base("name=LTQLDbcontext")
+        {
+        }
+        public virtual DbSet<Account> Account { get; set; }
+       
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+            .Property(e => e.UserName)
+            .IsUnicode(false);
+            modelBuilder.Entity<Account>().Property(e => e.Password)
+            .IsUnicode(false);
+
+           
+        }
+    }
+}
